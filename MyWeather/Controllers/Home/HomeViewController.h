4b618d1843815d6c0699c3e88e3f104faf13eb68
LucaAnto01 <luca.antognarelli@gmail.com>
2022-07-25 21:46:37 +0200

    Folder structure creation

---
+++ MyWeather/Controllers/Home/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  MyWeather
+//
+//  Created by Luca on 25/07/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyWeather/Controllers/Home/HomeViewController.m
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.m
+//  MyWeather
+//
+//  Created by Luca on 25/07/22.
+//
+
+#import "HomeViewController.h"
+
+/**Created for home view*/
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
