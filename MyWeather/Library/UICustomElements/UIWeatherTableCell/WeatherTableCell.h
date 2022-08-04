83b8f9be3c5d1b4315a10eb9136270edf1eb4778
LucaAnto01 <luca.antognarelli@gmail.com>
2022-08-04 10:46:26 +0200

    Creation personal Weather cell

---
+++ MyWeather/Library/UICustomElements/UIWeatherTableCell/WeatherTableCell.h
@@ -0,0 +1,23 @@
+//
+//  WeatherTableCell.h
+//  MyWeather
+//
+//  Created by Luca on 30/07/22.
+//
+
+#import <UIKit/UIKit.h>
+#import "../../../Models/MDCoordinate.h"
+#import "../../../Models/MDWeather.h"
+
+@interface WeatherTableCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *lbCity;
+@property (weak, nonatomic) IBOutlet UILabel *lbWeather;
+@property (weak, nonatomic) IBOutlet UILabel *lbMaxTemperature;
+@property (weak, nonatomic) IBOutlet UILabel *lbMinTemperature;
+
+@property (nonatomic, strong) MDWeather *weather;
+@property (nonatomic, strong) MDCoordinate *coordinate;
+
+
+@end
---
+++ MyWeather/Library/UICustomElements/UIWeatherTableCell/WeatherTableCell.m
@@ -0,0 +1,22 @@
+//
+//  WeatherTableCell.m
+//  MyWeather
+//
+//  Created by Luca on 30/07/22.
+//
+
+#import "WeatherTableCell.h"
+
+@implementation WeatherTableCell
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+}
+
+@end
