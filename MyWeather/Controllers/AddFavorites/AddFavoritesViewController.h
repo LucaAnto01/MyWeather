c2972635666f97713dbc28bbbfe55f4974b434e0
LucaAnto01 <luca.antognarelli@gmail.com>
2022-08-04 10:46:03 +0200

    Creation Add Favorites ViewController

---
+++ MyWeather/Controllers/AddFavorites/AddFavoritesViewController.h
@@ -0,0 +1,16 @@
+//
+//  AddFavoritesViewController.h
+//  MyWeather
+//
+//  Created by Luca on 03/08/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddFavoritesViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyWeather/Controllers/AddFavorites/AddFavoritesViewController.m
@@ -0,0 +1,32 @@
+//
+//  AddFavoritesViewController.m
+//  MyWeather
+//
+//  Created by Luca on 03/08/22.
+//
+
+#import "AddFavoritesViewController.h"
+
+@interface AddFavoritesViewController ()
+
+- (IBAction)tbCity:(id)sender;
+
+@end
+
+@implementation AddFavoritesViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)tbCity:(id)sender
+{
+}
+
+- (IBAction)btAddClick:(id)sender
+{
+}
+
+@end
